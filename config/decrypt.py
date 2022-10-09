8b6e50047909687450e474a8fad99b33627beac4
notPlancha <andre.plancha@hotmail.com>
2022-10-09 02:45:22 +0100

    added encryption

--- config/decrypt.py
+++ config/decrypt.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import getpass
 from pyAesCrypt import decryptFile as decrypt
 
 # args should be 'file password'
@@ -7,6 +8,6 @@ args = sys.argv
 if len(args) < 3:
     if len(args) == 1:
         args.append(input("File to decrypt: "))
-    args.append(input("Password: "))
+    args.append(getpass.getpass("Password: "))
 decrypt(args[1], args[1] + ".aes", args[2], bufferSize=64 * 1024)
 print("Encrypted file: " + args[1] + ".aes")
--- config/encrypt.py
+++ config/encrypt.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import getpass
 from pyAesCrypt import encryptFile as encrypt
 
 # args should be 'file password'
@@ -7,6 +8,6 @@ args = sys.argv
 if len(args) < 3:
     if len(args) == 1:
         args.append(input("File to encrypt: "))
-    args.append(input("Password: "))
+    args.append(getpass.getpass("Password: "))
 encrypt(args[1], args[1] + ".aes", args[2], bufferSize=64 * 1024)
 print("Encrypted file: " + args[1] + ".aes")
