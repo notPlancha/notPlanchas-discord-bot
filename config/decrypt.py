2497d5eba4f14fab96a32531d869461e334230ba
notPlancha <andre.plancha@hotmail.com>
2022-10-11 01:49:27 +0100

    updated secrets

--- config/decrypt.py
+++ config/decrypt.py
@@ -9,5 +9,5 @@ if len(args) < 3:
     if len(args) == 1:
         args.append(input("File to decrypt: "))
     args.append(getpass.getpass("Password: "))
-decrypt(args[1], args[1] + ".aes", args[2], bufferSize=64 * 1024)
-print("Encrypted file: " + args[1] + ".aes")
+decrypt(args[1], args[1][:-4], args[2], bufferSize=64 * 1024)
+print("Decrypted file: " + args[1][:-4])
--- config/encrypt.py
+++ config/encrypt.py
@@ -9,5 +9,5 @@ if len(args) < 3:
     if len(args) == 1:
         args.append(input("File to encrypt: "))
     args.append(getpass.getpass("Password: "))
-encrypt(args[1], args[1] + ".aes", args[2], bufferSize=64 * 1024)
+encrypt(args[1], args[1] + ".aes", args[2], bufferSize=64 * 1024, )
 print("Encrypted file: " + args[1] + ".aes")
